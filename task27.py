102b5415facaebd8eb7a22dc80f8ab15044ce54d
Barabasha <aleksrmalyshev@gmail.com>
2017-04-30 16:50:37 +0300

    Add files via upload

--- task27.py
+++ task27.py
@@ -3,8 +3,15 @@ lst = []
 for i in range(1,100,2):
     lst.append (i)
 print(lst)
-random.shuffle(lst)
+
+for i in range(0,len(lst)-1): #число замен у нас меньше на 1, чем длина строки
+    r = random.randint(i+1,len(lst)-1) #выбираем индекс, кроме уже использованных
+    x = lst[i] #далее меняем местами
+    lst[i] = lst[r]
+    lst[r] = x
 print(lst)
+
+
 
 
 
