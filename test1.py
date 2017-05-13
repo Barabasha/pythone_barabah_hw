3793c42512f60b592e4a26f97b030092939b8919
Barabasha <aleksrmalyshev@gmail.com>
2017-05-13 16:48:13 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+#(a + b * c)^2
+print("(a + b * c)^2")
+a = int(input("Enter number a"))
+b = int(input("Enter number b"))
+c = int(input("Enter number c"))
+result = (a + b * c)**2
+print("(%d + %d * %d)^2 = %d" %(a,b,c,result) )
