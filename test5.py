39696f6aef365a8dc4480080488395348138f621
Barabasha <aleksrmalyshev@gmail.com>
2017-05-14 12:51:54 +0300

    magic number

--- test5.py
+++ test5.py
@@ -1,13 +1,14 @@
 #Создать программу, выводящую на экран ближайшее к 10 из двух чисел, введенных пользователем.
-def near(a,b):
-    if abs(10-a)<abs(10-b):
+def near(a,b,number):
+    if abs(number-a)<abs(number-b):
         return a
-    elif abs(10-a)>abs(10-b):
+    elif abs(number-a)>abs(number-b):
         return b
     else:
         return 'Distances equal'
 
 a = float(input("Enter the first number"))
 b = float(input("Enter the second number"))
-print(near(a,b))
+number = 10
+print(near(a,b,number))
 
