929184e01cc00a106a5f3bc0b7995313dd69a8d2
Barabasha <aleksrmalyshev@gmail.com>
2017-05-15 17:27:23 +0300

    Add files via upload

--- test10.py
+++ test10.py
@@ -13,26 +13,17 @@ def print_table(table):
         print()
     return
 
-def table_revers(table):
-    for idx1 in range(line_reverse):
-        for idx2 in range(column):
-            elem = table[idx1][idx2]
-            table[idx1][idx2] = table[line - idx1 - 1][idx2]
-            table[line - idx1 - 1][idx2] = elem
+def matrix_revers(matrix):
     for idx1 in range(line):
-        for idx2 in range(column_reverse):
-            elem = table[idx1][idx2]
-            table[idx1][idx2] = table[idx1][column - idx2 - 1]
-            table[idx1][column - idx2 - 1] = elem
-    return table
+        for idx2 in range(column):
+            t_matrix [idx2][idx1] = matrix[idx1][idx2]
+    return t_matrix
 
 line = 3
 column = 8
-lst = [[0]*column for i in range(line)]
-random_table(lst)
-print_table(lst)
+matrix = [[0]*column for i in range(line)]
+t_matrix = [[0]*line for i in range(column)]
+random_table(matrix)
+print_table(matrix)
 print()
-line_reverse = int(line/2)
-column_reverse = int(column/2)
-table_revers (lst)
-print_table(lst)
+print_table(matrix_revers (matrix))
