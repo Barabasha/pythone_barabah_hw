6c048da188401619345fffa7d45eb4a262c45936
Barabasha <aleksrmalyshev@gmail.com>
2017-05-28 01:33:50 +0300

    task36

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# Создать два класса: Окружность и Точка.
-# Создать в классе окружности метод, который принимает в качестве параметра точку
-# и проверяет находится ли данная точка внутри окружности.
-
-import circle
-import point
+# Создать класс Транспортное средство и его потомков - классы Поезд и Самолет.
+# В родительском классе должно быть определено минимум 1 инициализатор, 3 атрибута и 1 метод.
+# В классах-потомках должны быть добавлены минимум по 1 новому методу и по 1 новому атрибуту.
 
 if __name__ == "__main__":
-    my_circle = circle.Circle(0,0,5)
-    my_point = point.Point (6,5)
-    my_circle.print_info()
-    my_point.print_info ()
+    import transport
+    import train
+    import plane
 
-if my_circle.is_point_in_circle(my_point) == True:
-    print ("The point is in a circle")
-else :
-    print("The point is not in a circle")
+    t1 = train.Train ("ЧМЭЗ", "passenger", 95, "disel")
+    t2 = train.Train ("ЭЭЛ", "cargo", 110, "electric")
+    p1= plane.Plane ("МиГ-35", "weapons", 2560, "military")
+    p2 = plane.Plane("Boeing 747-8", "passenger", 1150, "civil")
+    train.Train.print_info(t1)
+    train.Train.print_info(t2)
+    plane.Plane.print_info (p1)
+    plane.Plane.print_info (p2)
