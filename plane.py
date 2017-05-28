ac91a365e384588a4577e1004df54c55f78fbc8e
Barabasha <aleksrmalyshev@gmail.com>
2017-05-28 12:10:17 +0300

    Add files via upload

--- plane.py
+++ plane.py
@@ -1,10 +1,17 @@
 import transport
-class Plane:
+class Plane (transport.Transport):
 
     def __init__(self, model, type_cargo, maximum_speed, assignment):
-        transport.Transport.__init__(self, model, type_cargo, maximum_speed)
+        super().__init__(model, type_cargo, maximum_speed)
         self.assignment = assignment
 
     def print_info(self):
-        transport.Transport.print_info(self)
+        super().print_info()
         print(transport.Transport.FORMAT_ALL % ("Assignment:   ", self.assignment))
+
+    def speed_comparison(plane1,plane2):
+        print()
+        if plane1.maximum_speed > plane2.maximum_speed :
+            print("%s faster than %s for %s km/hr" %(plane1.model, plane2.model, plane1.maximum_speed-plane2.maximum_speed))
+        else :
+            print("%s faster than %s for %s km/hr" %(plane2.model, plane1.model, plane2.maximum_speed-plane1.maximum_speed))
