1b2683631c3045c2d0dfde80fd937ceb56e6e314
Barabasha <aleksrmalyshev@gmail.com>
2017-05-28 12:10:05 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -2,10 +2,19 @@ import transport
 class Train (transport.Transport):
 
     def __init__(self, model, type_cargo, maximum_speed, engine_type):
-        transport.Transport.__init__(self, model, type_cargo, maximum_speed)
+        super().__init__(model, type_cargo, maximum_speed)
         self.engine_type = engine_type
 
+
     def print_info(self):
-        transport.Transport.print_info(self)
+        super().print_info()
         print(transport.Transport.FORMAT_ALL %("Engine_type:  ", self.engine_type))
+
+    def is_type_cargo_passenger(self):
+        print()
+        if self.cargo == "passenger":
+            print(self.model, "is passenger")
+        else:
+            print(self.model, "is not passenger")
+
 
